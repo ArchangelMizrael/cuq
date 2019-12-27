1b2ccbfddfeaee28076962096e0282f4540892fb
ArchangelMizrael <43006439+ArchangelMizrael@users.noreply.github.com>
2019-12-27 14:07:21 +0300

    Update device_selection.cu

--- src/device_selection.cu
+++ src/device_selection.cu
@@ -79,14 +79,12 @@ vector<int> readCudaVisibleDevices() {
     if (ss.peek() == ',' || ss.peek() == '"' || ss.peek() == '\'')
       ss.ignore();
   }
-
-  print("\n\n============================\n\n res = ", res);
-  print("\n ss = \n\n============================\n\n", ss);
   
-//  cout << "\n\n============================\n\n";
-//  for (vector<char>::const_iterator i = res.begin(); i != res.end(); ++i)
-//    cout << *i << ' ';
-//  cout << "\n\n============================\n\n";
+  cout << "\n\n============================\n\n";
+  for (int i = 0; i < res.size(); i++) {
+		cout << res.at(i) << ' ';
+	}
+  cout << "\n\n============================\n\n";
   
   return res;
 }
